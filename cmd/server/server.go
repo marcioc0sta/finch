61fcbf4c1fd62a1b92efb66e8ddbfd4aad5c8b4b
Marcio Costa <marciocst2@gmail.com>
2023-04-23 18:14:16 -0300

    feat: add configs

---
+++ cmd/server/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/marcioc0sta/finch/configs"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("finch.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/", nil)
+
+	http.ListenAndServe(":8000", r)
+}
